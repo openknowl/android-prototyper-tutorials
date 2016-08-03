7cc8483b92d928d7e48f8faeeb211b0d02e6c5b7
gracefulife <gracefulife@naver.com>
2016-08-03 16:14:59 +0900

    안드로이드 강의 내용 보충

---
+++ android/android_tutorial_1/HelloAndroid/app/src/main/java/com/openknowl/helloandroid/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.openknowl.helloandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gracefulife on 16. 8. 3.
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- android/android_tutorial_1/HelloAndroid/app/src/main/java/com/openknowl/helloandroid/Subject.java
+++ android/android_tutorial_1/HelloAndroid/app/src/main/java/com/openknowl/helloandroid/Subject.java
@@ -2,7 +2,8 @@ package com.openknowl.helloandroid;
 
 public class Subject {
     /**
-     * 예제에서 배운것을 이용해서 첫 화면을 ATM 기기처럼 꾸며보자.
+     * 예제에서 배운것을 이용해서 계좌 번호를 이용하여 로그인을 하는 화면을 만들고,
+     * 로그인 시 나오는 화면을 ATM 기기처럼 꾸며보자.
      * 편의점 앞의 ATM 기기를 생각해보면 UI를 쉽게 구상할 수 있지않을까.
      */
 }
