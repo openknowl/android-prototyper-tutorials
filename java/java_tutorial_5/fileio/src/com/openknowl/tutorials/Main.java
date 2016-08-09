b9db1c84bb666a2c25de626af26b41543080d61d
gracefulife <gracefulife@naver.com>
2016-08-09 13:31:37 +0900

    fileio 예제 수정
    1. readLines 메소드에서 fileName 인자를 안쓰던 것을 수정하였음.
    2. lambda 식으로 된 것을 for문으로 변경.

--- java/java_tutorial_5/fileio/src/com/openknowl/tutorials/Main.java
+++ java/java_tutorial_5/fileio/src/com/openknowl/tutorials/Main.java
@@ -1,6 +1,8 @@
 package com.openknowl.tutorials;
 
 import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -13,10 +15,14 @@ public class Main {
         // Input stream
         try {
             BufferedReader stream = new BufferedReader(new FileReader(fileName));
-            stream.lines().forEach((line) -> {
-                System.out.println(line);
-            });
+//            stream.lines().forEach((line) -> {
+//                System.out.println(line);
+//            });
 
+            List<String> list = stream.lines().collect(Collectors.toList());
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -27,7 +33,7 @@ public class Main {
     public static void readLines(String fileName) {
         // Ouput stream
         try {
-            FileOutputStream stream = new FileOutputStream("test.dat");
+            FileOutputStream stream = new FileOutputStream(fileName);
             for (int i = 0; i < 11; i++) {
                 String data = i + " 번쨰 줄입니다.\n";
                 stream.write(data.getBytes());
