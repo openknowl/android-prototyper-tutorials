80cf3e5e09610d5cbade437aca71eef45ee44a90
gracefulife <gracefulife@naver.com>
2016-08-10 19:17:46 +0900

    14. 레이아웃 심화 + 컨텐트 프로바이더 + 인텐트
    전화번호부 예제를 통한 레이아웃 연습과
    컨텐트 프로바이더 및 인텐트 사용방법 학습.
    6.0 버전 이상 퍼미션 처리

---
+++ android/android_tutorial_3/Ontact/app/src/main/java/com/openknowl/ontact/adapter/ContactListAdapter.java
@@ -0,0 +1,74 @@
+package com.openknowl.ontact.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.openknowl.ontact.R;
+import com.openknowl.ontact.model.Contact;
+
+
+/**
+ * Created by gracefulife on 16. 8. 9.
+ */
+public class ContactListAdapter extends ArrayAdapter<Contact> {
+    private Contact[] contacts;
+
+    public ContactListAdapter(Context context, int resource, Contact[] contacts) {
+        super(context, resource, contacts);
+        this.contacts = contacts;
+
+
+    }
+
+    static class ViewHolder implements View.OnClickListener {
+        ImageView avatarImageView;
+        TextView nameTextView;
+        ImageView outgoingCallImageView;
+        Contact contact;
+
+
+        @Override
+        public void onClick(View view) {
+            Uri uri = Uri.parse("tel:" + contact.getNumber());
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            view.getContext().startActivity(intent);
+        }
+
+        public void init(Contact[] contacts, int position) {
+            this.contact = contacts[position];
+            nameTextView.setText(contact.getName());
+            outgoingCallImageView.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_contact, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.avatarImageView = (ImageView) convertView.findViewById(R.id.avatarImageView);
+            viewHolder.nameTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+            viewHolder.outgoingCallImageView = (ImageView) convertView.findViewById(R.id.outgoingCallImageView);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.init(contacts, position);
+
+        return convertView;
+    }
+
+}
