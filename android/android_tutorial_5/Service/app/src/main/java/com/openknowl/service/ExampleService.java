7c5aa04dbf72d7bfe8877d0c8e62f9190265de5d
gracefulife <gracefulife@naver.com>
2016-08-17 16:52:54 +0900

    service 예제 수정

--- android/android_tutorial_5/Service/app/src/main/java/com/openknowl/service/ExampleService.java
+++ android/android_tutorial_5/Service/app/src/main/java/com/openknowl/service/ExampleService.java
@@ -2,19 +2,23 @@ package com.openknowl.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
  * Created by gracefulife on 16. 8. 17.
  */
 public class ExampleService extends IntentService {
+    private static final String TAG = ExampleService.class.getSimpleName();
+
     public ExampleService() {
         super("ExampleService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Toast.makeText(getApplicationContext(), "서비스 실행되었음 !", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), "서비스 실행되었음 !", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "서비스 실행되었음 !");
     }
 }
 
