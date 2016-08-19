157cce6f81853bf54fcc82ad8df46a45fe0e8e54
gracefulife <gracefulife@naver.com>
2016-08-19 19:22:32 +0900

    22. OkHttp 예제

---
+++ android/android_tutorial_8/OkHttpExample/app/src/main/java/com/openknowl/okhttpexample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.openknowl.okhttpexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/users/KimKyung-man")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i(TAG, "" + response.body().string());
+            }
+        });
+
+    }
+}
